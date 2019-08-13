db4deab9c9aaf1de92cac16fc01a4dbb7836e036
Luis03 <rage.875@gmail.com>
2019-08-12 21:47:27 -0500

    Updates

---
+++ precache-manifest.865aeaaa57f484fb81ec3c5c9ed40ba1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbb877e40cad5211881080a15439c53",
+    "url": "/Project_FS_CF/index.html"
+  },
+  {
+    "revision": "5508d6ad258354383515",
+    "url": "/Project_FS_CF/static/css/main.a3866baa.chunk.css"
+  },
+  {
+    "revision": "2cae0bf6907dc5ebe450",
+    "url": "/Project_FS_CF/static/js/2.4026a459.chunk.js"
+  },
+  {
+    "revision": "5508d6ad258354383515",
+    "url": "/Project_FS_CF/static/js/main.b148f940.chunk.js"
+  },
+  {
+    "revision": "7baf441eea3f7b82e0f8",
+    "url": "/Project_FS_CF/static/js/runtime~main.f1967c3e.js"
+  }
+]);
